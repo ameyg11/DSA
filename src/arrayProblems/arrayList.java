3bb1144ba475f1462baff082342355a2b5ecfc2e
Amey <ameygawade11@gmail.com>
2024-11-20 00:15:09 +0530

    new problem

--- src/arrayProblems/arrayList.java
+++ src/arrayProblems/arrayList.java
@@ -1,5 +1,20 @@
 package arrayProblems;
 import java.util.ArrayList;
+import java.util.Collections;
+
+class arrayLst{
+    static void reverseArray(ArrayList<Integer> array){
+        int i = 0 , j = array.size()-1;
+
+        while(i < j){
+            Integer temp = Integer.valueOf(array.get(i));
+            array.set(i,array.get(j));
+            array.set(j,temp);
+            i++;
+            j--;
+        }
+    }
+}
 
 public class arrayList {
     public static void main(String[] args) {
@@ -41,6 +56,28 @@ public class arrayList {
         //removing element at index i
         in.remove(1);
         System.out.println(in);
+
+        //removing an element e
+        in.remove(Integer.valueOf(3));
+        System.out.println(in);
+
+        ArrayList <Integer> array = new ArrayList<>();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+
+        System.out.println("Original list" +array);
+
+        arrayLst.reverseArray(array);
+
+        System.out.println("Reversed List" +array);
+
+        Collections.reverse(array);
+        System.out.println("Collection reversed list" +array);
+
+
 
 
     }
