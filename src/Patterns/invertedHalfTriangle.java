f649397c0d8739ec0ecdb77be065a7990cf26780
Amey <ameygawade11@gmail.com>
2025-05-02 21:33:22 +0530

    created a patterns folder and solved 2 pattern questions

---
+++ src/Patterns/invertedHalfTriangle.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class invertedHalfTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = n; i > 0; i--){
+            for (int j = n; j < i; j++){
+                System.out.print(" ");
+            }
+            for (int k = i; k > 0; k--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
