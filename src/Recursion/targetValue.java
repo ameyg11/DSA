294d3c77853ca3b61d19e511842222b0e2a5965b
Amey <ameygawade11@gmail.com>
2025-05-26 22:41:53 +0530

    linear search on arrays using recursion print all the indices occured

--- src/Recursion/targetValue.java
+++ src/Recursion/targetValue.java
@@ -2,6 +2,29 @@ package Recursion;
 
 public class targetValue {
 
+
+    // print all the indices occured
+    public static void printIndices(int[] arr, int val, int idx){
+
+        //  base case
+        if(idx > arr.length - 1){
+            return;
+        }
+        // self work
+        if(arr[idx] == val){
+            System.out.println(idx);
+        }
+
+        // recursive work
+        printIndices(arr, val, idx +1);
+
+//        if(findValue(arr, val, idx +1)){
+//            return true;
+//        }else {
+//            return false;
+//        }
+
+    }
     public static boolean findValue(int[] arr, int val, int idx){
 
         // self work
@@ -25,9 +48,10 @@ public class targetValue {
 
     }
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        int idx = 0, value = 2;
+        int[] arr = {1,2,3,3,4,3};
+        int idx = 0, value = 3;
 
         System.out.println(findValue(arr,value,idx));
+        printIndices(arr,value,idx);
     }
 }
