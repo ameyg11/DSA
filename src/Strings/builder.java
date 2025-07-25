84b9df9e0b08db6530b8c4103c7d656d1b6efb3b
Amey <ameygawade11@gmail.com>
2025-07-25 10:39:15 +0530

    stringBuilder concept completed

---
+++ src/Strings/builder.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class builder {
+    public static void main(String[] args) {
+
+        //<---------- declaration and initialization --------------------->
+        StringBuilder str = new StringBuilder("hello");
+        StringBuilder str2 = new StringBuilder();
+        StringBuilder str3 = new StringBuilder(100);
+    }
+}
