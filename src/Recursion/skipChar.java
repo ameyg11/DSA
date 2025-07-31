e235a8ade786098627f8cc27e9f877d089bf5e7b
Amey <ameygawade11@gmail.com>
2025-07-31 11:18:17 +0530

    solved the recursion question on skipping character in a string

---
+++ src/Recursion/skipChar.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class skipChar {
+    public static void main(String[] args) {
+        System.out.println(skippedStr("bcabaacega", 'a', 0, ""));
+    }
+
+    public static String skippedStr(String str, char ch, int idx, String ans){
+        if(idx >= str.length()){
+            return ans;
+        }
+
+        if(str.charAt(idx) != ch){
+            ans += str.charAt(idx);
+//            return skippedStr(str, ch, idx + 1, ans += str.charAt(idx));
+//            System.out.println(ans);
+        }
+
+
+
+        return skippedStr(str, ch, idx + 1, ans);
+    }
+}
