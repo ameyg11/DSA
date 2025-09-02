4949dc9d24035e087d2d34adb8c2a5eb84cd8dbb
Amey <ameygawade11@gmail.com>
2025-09-03 00:45:08 +0530

    started learning the HashMaps

---
+++ src/commonQue/hashMap1.java
@@ -0,0 +1,29 @@
+package commonQue;
+import java.util.*;
+
+public class hashMap1 {
+    public static void main(String[] args) {
+        HashMap <String, Integer> map = new HashMap<>();
+
+        map.put("amey", 11);
+        map.put("tejas", 12);
+        map.put("siddesh", 10);
+
+        System.out.println(map.get("amey"));
+        System.out.println(map.containsValue(11));
+
+        // mostly reccomended
+        for(Map.Entry<String, Integer> e : map.entrySet()){
+            System.out.print(e.getKey());
+            System.out.println(e.getValue());
+        }
+
+        // 2nd way to iterate
+
+        Set<String> keys = map.keySet();
+        for(String key: keys){
+            System.out.print(" " + key + " " +map.get(key));
+        }
+
+    }
+}
