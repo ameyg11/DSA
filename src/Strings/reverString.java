8310c0ede1b74fb6394cd3a9b917c0b11dbdc228
Amey <ameygawade11@gmail.com>
2025-09-27 23:08:12 +0530

    string question

---
+++ src/Strings/reverString.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class reverString {
+    public static void main(String[] args) {
+        String st = "School";
+
+        int i=0, j=st.length() -1;
+
+        while(i<j){
+            char temp = st.charAt(j);
+
+        }
+    }
+}
