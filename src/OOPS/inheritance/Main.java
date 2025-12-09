8f27c088c1749b63b95f0990286d6b9af4fe6eab
Amey <ameygawade11@gmail.com>
2025-12-09 08:57:21 +0530

    revised some OOPS concepts

---
+++ src/OOPS/inheritance/Main.java
@@ -0,0 +1,14 @@
+package OOPS.inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+//        Box box = new Box(2);
+//        Box box2 = box;
+//
+//        System.out.println(box.l);
+
+        BoxWeight box = new BoxWeight(2,3,4,5);
+        System.out.println(box.h);
+
+    }
+}
---
+++ src/arrayProblems/nextGreaterElement.java
@@ -0,0 +1,33 @@
+package arrayProblems;
+
+import java.util.Arrays;
+
+public class nextGreaterElement {
+    public static void main(String[] args) {
+        int[] nums1 = {4,1,2};
+        int[] nums2 = {1,3,4,2};
+        System.out.println(Arrays.toString(nextGreaterElement(nums1, nums2)));
+    }
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+
+        for(int i=0; i<nums1.length; i++){
+            res[i] = -1;
+            int k=0;
+            while(k < nums2.length){
+                if(nums1[i] == nums2[k]){
+                    break;
+                }
+                k++;
+            }
+            for(int j=k+1; j<nums2.length; j++){
+                if(j < nums2.length && nums1[i] < nums2[j]){
+                    res[i] = nums2[j];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
